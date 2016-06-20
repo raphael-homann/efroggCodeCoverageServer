6ff709cd4240c1d994eb8c3f91ccf003d6ba7c28
Raphael <raph@e-frogg.com>
2016-06-20 15:59:18 +0200

    add Storage

---
+++ src/Coverage/Storage/CoverageStorage.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: raph
+ * Date: 20/06/16
+ * Time: 15:54
+ */
+
+namespace efrogg\Coverage\Storage;
+
+class CoverageStorage {
+    public function ()
+    {
+
+    }
+}
